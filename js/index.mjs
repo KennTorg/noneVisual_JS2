17b5253b9be1a9ca70ea08ca9eb58bec945bee0f
Kennet Torgersen <91532197+KennTorg@users.noreply.github.com>
2022-12-15 11:33:22 +0100

    paths

---
+++ js/index.mjs
@@ -0,0 +1,11 @@
+import * as listeners from "./handlers/index.mjs";
+import * as postMethods from "./api/posts/index.mjs";
+import * as templates from "./templates/index.mjs";
+
+const path = location.pathname;
+
+if (path === "/profile/login/") {
+  listeners.setLoginFormListener();
+} else if (path === "/profile/register/") {
+  listeners.setRegisterFormListener();
+}
