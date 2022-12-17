b1e0bf207bad9fc90f8fcc182d72abe46d8724d1
Kennet Torgersen <91532197+KennTorg@users.noreply.github.com>
2022-12-17 14:59:51 +0100

    redirect

--- js/index.mjs
+++ js/index.mjs
@@ -8,4 +8,58 @@ if (path === "/profile/login/") {
   listeners.setLoginFormListener();
 } else if (path === "/profile/register/") {
   listeners.setRegisterFormListener();
+} else if (path === "/profile/") {
+  postMethods.getPosts();
+  postMethods.getPost(1);
+  listeners.searchPosts();
+  templates.postTemplate();
+  templates.renderPostTemplate();
+  templates.renderPostTemplates();
+  listeners.functionA();
+  listeners.functionB();
 }
+
+// ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ CONSOLE TESTING! ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ //
+
+// CREATE POST TEST --- WORKING!!
+
+/* createPost({
+  title: "Its snowing!",
+  body: "So much!!!!!",
+});
+ */
+
+// UPDATE POST TEST --- WORKING!!
+
+/* updatePost({
+  id: 1,
+  title: "Its snowing! UPDATED!",
+  body: NOT so much!!!,
+});
+*/
+
+// DELETE POST --- WORKING!!
+
+// removePost(1);
+
+// GET POSTS --- WORKING
+
+//postMethods.getPosts().then(console.log);
+
+// GET POST BY ID --- WORKING
+
+// postMethods.getPost(1).then(console.log);
+
+/* async function testTemplate() {
+  const posts = await postMethods.getPosts();
+  const post = posts.pop()
+  renderPostTemplate(post,)
+} */
+
+// SEARCH
+
+// searchPosts();
+
+// SORT
+//
+//
